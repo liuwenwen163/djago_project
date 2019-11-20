e936bbf28693d1b5dcbefde3feb9174c9b020b45
liuwenwen163 <liuwenwen163@gamil.com>
2019-11-20 16:04:10 +0800

    4-2 列方法与属性的代码

--- django_learn/orm_learn/app/models.py
+++ django_learn/orm_learn/app/models.py
@@ -1,6 +1,17 @@
 # coding:utf-8
-from django.db import models
+from django.db.models import Model,CharField,IntegerField,SmallIntegerField,EmailField,TextField,DateTimeField,BigIntegerField
 
-class Test(models.Model):
-    name = models.CharField(max_length=20)
-    ages = models.IntegerField()
+class Test(Model):
+    name = CharField(max_length=20)
+    ages = IntegerField()
+
+
+class User(Model):
+    id = IntegerField(primary_key=True)
+    username = CharField(unique=True, max_length=50, blank=False)
+    age = SmallIntegerField(default=0)
+    phone = BigIntegerField(db_index=True, blank=True, default=0)
+    email = EmailField(blank=True, default='')
+    info = TextField()
+    create_time = DateTimeField(auto_now_add=True)
+    update_time = DateTimeField(auto_now=True)
