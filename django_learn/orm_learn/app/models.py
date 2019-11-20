c6d5b7c377b0f9a57569d085c42841b9935287ed
liuwenwen163 <liuwenwen163@gamil.com>
2019-11-20 19:16:33 +0800

    4-3 表关系与联合索引

--- django_learn/orm_learn/app/models.py
+++ django_learn/orm_learn/app/models.py
@@ -1,5 +1,7 @@
 # coding:utf-8
+from django.db import models
 from django.db.models import Model,CharField,IntegerField,SmallIntegerField,EmailField,TextField,DateTimeField,BigIntegerField
+
 
 class Test(Model):
     name = CharField(max_length=20)
@@ -15,3 +17,33 @@ class User(Model):
     info = TextField()
     create_time = DateTimeField(auto_now_add=True)
     update_time = DateTimeField(auto_now=True)
+
+    class Meta:
+        index_together = ['username', 'phone']
+
+
+class Userprofile(Model):
+    """创建一个一对一的实例"""
+    id = IntegerField(primary_key=True)
+    user = models.OneToOneField(User, blank=True, null=True, on_delete=models.SET_NULL)
+    birthday = CharField(max_length=100, blank=True, default='')
+
+
+class Diary(Model):
+    """创建一个一对多的实例"""
+    id = models.IntegerField(primary_key=True)
+    user = models.ForeignKey(User, related_name='diary',
+                             on_delete=models.SET_NULL,
+                             blank=True, null=True)
+    content = models.TextField()
+    # 存时间戳到数据库里
+    create_time = models.IntegerField()
+
+
+class Group(Model):
+    """多对多关系的表结构，一个用户在多个组里，一个组里有多个用户"""
+    id = models.IntegerField(primary_key=True)
+    user = models.ManyToManyField(User, related_name='group')
+    name = models.CharField(max_length=20)
+    create_time = IntegerField()
+
