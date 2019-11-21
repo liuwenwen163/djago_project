a4fae1760d34b98f21b92768bf966355451bf256
liuwenwen163 <liuwenwen163@gamil.com>
2019-11-21 19:25:42 +0800

    4-9 练习创建mysql数据库message_test.然后migrate迁移创建成功

--- django_project_local/message_test/app/models.py
+++ django_project_local/message_test/app/models.py
@@ -1,3 +1,13 @@
+# coding:utf-8
 from django.db import models
 
-# Create your models here.
+
+class Message(models.Model):
+    content = models.TextField()
+    message_type = models.CharField(max_length=10, db_index=True)
+    created_time = models.IntegerField(default=0)
+
+    def __str__(self):
+        return 'type:{}, content:{}'.format(self.message_type, self.content)
+
+
