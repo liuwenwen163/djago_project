8de0303528de76d3ec4caf79f8617c980f42f4ef
liuwenwen163 <liuwenwen163@gamil.com>
2019-11-22 17:36:26 +0800

    5-1 普通的前端定义表单后端接收示例代码

---
+++ django_learn/form/app/urls.py
@@ -0,0 +1,8 @@
+# encoding: utf-8
+from django.urls import path
+
+from .views import Regist
+
+urlpatterns = [
+    path('regist', Regist.as_view(), name='regist')
+]
--- django_learn/form/app/views.py
+++ django_learn/form/app/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+# coding:utf-8
+from django.shortcuts import render, redirect
+from django.views.generic import View
 
-# Create your views here.
+class Regist(View):
+    TEMPLATE = 'regist.html'
+
+    def get(self, request):
+        return render(request, self.TEMPLATE)
+
+    def post(self, request):
+        # 这里括号中的username对应前端input标签中的name属性
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        print('username', username)
+        print('password', password)
+        return redirect('/regist')
+
+
