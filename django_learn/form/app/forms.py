a646a33a03197f0102dc314c3f073dcf9cce615a
liuwenwen163 <liuwenwen163@gamil.com>
2019-11-23 21:17:06 +0800

    5-3 结合使用模型表单，模型验证过后将数据存入数据库，或者从数据库中提取数据 的实例代码

--- django_learn/form/app/forms.py
+++ django_learn/form/app/forms.py
@@ -3,65 +3,121 @@
 
 from django import forms
 from django.forms import fields
+from .models import Auth as AuthModel
 
+class AuthModelForm(forms.ModelForm):
+    class Meta:
+        model = AuthModel
 
-class Auth(forms.Form):
-    username = fields.CharField(
-        # max_length=11,
-        required=False,
-        label="username",
-        widget=forms.TextInput(
-            attrs={'placeholder': '最大不可超过10个字符'}),
-    )
-    password = fields.CharField(
-        widget=forms.PasswordInput(attrs={'placeholder': '请输入密码'}),
-        label="password",
-        required=False,
-        min_length=10,
-        error_messages={'min_length': '最小不能低于10个字符'}
-    )
+        # 映射全部字段，使用 '__all__'
+        fields = ['username', 'password']
+        exclude = []  # 输入不转成表单字段的model字段
 
-    # def clean(self):
-    #     """
-    #     clean是统一验证
-    #     进行验证时，会自动调用 clean
-    #     """
-    #     # 如果没有获取到对应的字段，就赋予一个空值
-    #     username = self.cleaned_data.get('username', '')
-    #     password = self.cleaned_data.get('password', '')
-    #
-    #     if not username:
-    #         raise forms.ValidationError('用户名不可以为空')
-    #
-    #     if len(username) > 10:
-    #         raise forms.ValidationError('用户名最大不能超过10')
-    #
-    #     if not password:
-    #         raise forms.ValidationError('密码不可以为空')
+        # 定义字段的类型，一般会按照model的类型自动转换
+        field_classes = {
+            'username': forms.CharField,
+            'password': forms.CharField,
+        }
+
+        labels = {
+            'username': '用户名',
+            'password': '密码',
+        }
+
+        widgets = {
+            'username': forms.TextInput(
+                attrs={'placeholder': '请输入用户名'}
+            ),
+            'password': forms.PasswordInput(
+                attrs={'placeholder': '请输入密码'},
+                # 这个参数代表渲染值，默认是False
+                render_value=True
+            )
+        }
+
+        error_messages = {
+            'username': {'required': '用户名不可以为空'},
+            'password': {'min_length': '密码最少不可以低于10个字符'}
+        }
 
     def clean_username(self):
-        username = self.cleaned_data.get('username', '')
-
-        if not username:
-            raise forms.ValidationError('用户名不可以为空')
+        print('111')
+        username = self.cleaned_data.get('username')
 
         if len(username) > 10:
-            raise forms.ValidationError('用户名最大不能超过10')
+            raise forms.ValidationError('用户名最大不可超过10')
 
-    def clean(self):
-        """单独验证和集体验证结合"""
-        password = self.cleaned_data.get('password', '')
-
-        if not password:
-            raise forms.ValidationError('密码不可以为空')
+        return username
 
 
-    # def clean_password(self):
-    #     password = self.cleaned_data.get('password', '')
-    #
-    #     if not password:
-    #         raise forms.ValidationError('密码不可以为空')
-    #
-    #     if len(password) > 8:
-    #         raise forms.ValidationError('密码不能超过8位')
 
+
+# class Auth(forms.Form):
+#     username = fields.CharField(
+#         # max_length=11,
+#         required=False,
+#         label="username",
+#         widget=forms.TextInput(
+#             attrs={'placeholder': '最大不可超过10个字符'}),
+#     )
+#     password = fields.CharField(
+#         widget=forms.PasswordInput(attrs={'placeholder': '请输入密码'}),
+#         label="password",
+#         required=False,
+#         min_length=10,
+#         error_messages={'min_length': '最小不能低于10个字符'}
+#     )
+#
+#     # def clean(self):
+#     #     """
+#     #     clean是统一验证
+#     #     进行验证时，会自动调用 clean
+#     #     """
+#     #     # 如果没有获取到对应的字段，就赋予一个空值
+#     #     username = self.cleaned_data.get('username', '')
+#     #     password = self.cleaned_data.get('password', '')
+#     #
+#     #     if not username:
+#     #         raise forms.ValidationError('用户名不可以为空')
+#     #
+#     #     if len(username) > 10:
+#     #         raise forms.ValidationError('用户名最大不能超过10')
+#     #
+#     #     if not password:
+#     #         raise forms.ValidationError('密码不可以为空')
+#
+#     def clean_username(self):
+#         username = self.cleaned_data.get('username', '')
+#
+#         if not username:
+#             raise forms.ValidationError('用户名不可以为空')
+#
+#         if len(username) > 10:
+#             raise forms.ValidationError('用户名最大不能超过10')
+#
+#     def clean(self):
+#         """单独验证和集体验证结合"""
+#         password = self.cleaned_data.get('password', '')
+#
+#         if not password:
+#             raise forms.ValidationError('密码不可以为空')
+#
+#
+#     # def clean_password(self):
+#     #     password = self.cleaned_data.get('password', '')
+#     #
+#     #     if not password:
+#     #         raise forms.ValidationError('密码不可以为空')
+#     #
+#     #     if len(password) > 8:
+#     #         raise forms.ValidationError('密码不能超过8位')
+
+class A():
+    def __init__(self, x):
+        self.x = x
+
+class B():
+    def __init__(self, x):
+        self.x = x
+    def __str__(self):
+        return 'x value is:{}'.format(self.x)
--- django_learn/form/app/views.py
+++ django_learn/form/app/views.py
@@ -1,27 +1,33 @@
 # coding:utf-8
 from django.shortcuts import render, redirect
 from django.views.generic import View
-from .forms import Auth
+from .forms import AuthModelForm
+from .models import Auth as AuthModel
 
 
 class Regist(View):
     TEMPLATE = 'regist.html'
 
     def get(self, request):
-        form = Auth()
+        # 从数据获取数据
+        user = AuthModel.objects.filter(pk=3).first()
+        if user:
+            form = AuthModelForm(instance=user)
+        else:
+            form = AuthModelForm()
 
         return render(request, self.TEMPLATE, {'form': form})
 
     def post(self, request):
-        form = Auth(request.POST)
+        form = AuthModelForm(request.POST)
 
         if form.is_valid():
             # cleaned_data 代表经过验证之后，纯正的数据
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password')
 
-            print('username', username)
-            print('password', password)
+            # 如果表单验证成功了，就将数据存储到数据库
+            form.save()
         else:
             # 验证不通过，此时的form携带了non_field_errors，渲染错误消息
             return render(request, self.TEMPLATE, {'form': form})
